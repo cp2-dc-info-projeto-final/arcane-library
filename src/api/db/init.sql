2b60a8316321e1762ae684bbce9aefd4b19c5b97
miguel.santos.silva.07@gmail.com <miguel.santos.silva.07@gmail.com>
2026-05-05 17:12:34 -0300

    banco de dados

--- src/api/db/init.sql
+++ src/api/db/init.sql
@@ -4,16 +4,16 @@
 -- Criar usuário da aplicação se não existir
 DO $$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'elefantinho') THEN
-      CREATE USER elefantinho WITH ENCRYPTED PASSWORD 'elefantinho';
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'arcane_library') THEN
+      CREATE USER arcane_library WITH ENCRYPTED PASSWORD 'arcane_library';
    END IF;
 END
 $$;
 
--- Conceder permissões ao usuário no banco elefantinho
-\c elefantinho;
-GRANT ALL ON SCHEMA public TO elefantinho;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO elefantinho;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO elefantinho;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO elefantinho;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO elefantinho;
+-- Conceder permissões ao usuário no banco arcane_library
+\c arcane_library;
+GRANT ALL ON SCHEMA public TO arcane_library;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO arcane_library;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO arcane_library;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO arcane_library;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO arcane_library;
