f8265e5b1b533a534fcd1b40dddd177e43cae242
Mai Neime <labcaxias@LDC9C6B000EFEB8.labdc.zentyal>
2026-05-11 18:00:50 -0300

    perfis, usuario em andamento

--- src/api/db/create.sql
+++ src/api/db/create.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuario (
     login text NOT NULL,
     email text NOT NULL,
     senha text NOT NULL,
-    role text NOT NULL DEFAULT 'user',
+    role text NOT NULL DEFAULT 'cliente',
     
     -- Constraints
     CONSTRAINT pk_usuario PRIMARY KEY (id),
@@ -14,10 +14,12 @@ CREATE TABLE usuario (
     CONSTRAINT ck_usuario_login_length CHECK (length(login) >= 3 AND length(login) <= 50), -- comprimento
     CONSTRAINT ck_usuario_email_format CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'), -- formato de email com expressão regular
     CONSTRAINT ck_usuario_senha_length CHECK (length(senha) >= 6), -- comprimento mínimo
-    CONSTRAINT ck_usuario_role_valid CHECK (role IN ('admin', 'user')) -- tipos de usuário
+    CONSTRAINT ck_usuario_role_valid CHECK (role IN ('admin','bibliotecario','cliente')) -- tipos de usuário
 );
 
 INSERT INTO usuario (login, email, senha, role) VALUES
--- senha efelantinho
-('hermenegildo', 'hermenegildo@email.com', '$2a$12$f2c.uHGHS4drfaz6HR870OLamkarD57kI.gkr4//Vbbp0vN9IrFfG', 'admin'),
-('zoroastra', 'zoroastra@email.com', '$2a$12$f2c.uHGHS4drfaz6HR870OLamkarD57kI.gkr4//Vbbp0vN9IrFfG', 'user');
+-- senha 123456
+('Gui', 'gui@gmail.com', '$2a$12$PA7QHgIxNC8YO6.Og2IVTuVu55N4DHP3C95XtDyQ7BgsDc98nemtK', 'admin'),
+('Biel', 'biel@gmail.com', '$2a$12$PA7QHgIxNC8YO6.Og2IVTuVu55N4DHP3C95XtDyQ7BgsDc98nemtK', 'cliente');
+
+
